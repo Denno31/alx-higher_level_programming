ab96f552572619d38bdd32a3183708bad76c31eb
Denno31 <38795170+Denno31@users.noreply.github.com>
2022-01-12 20:36:41 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# curl size
-curl -i $1 | grep Content-Length | tail -c 4
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
